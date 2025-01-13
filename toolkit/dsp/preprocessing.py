8a435e69201ee1413b54ef90928063043a0755ff
Martin Esparza <martinesparza22.me@gmail.com>
2025-01-13 16:20:14 +0000

    Move init

---
+++ toolkit/dsp/preprocessing.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import pyaldata as pyal
+
+from toolkit.logging import set_logging
+from toolkit.params import Params
+
+logger = set_logging(__name__)
+
+
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Preprocessing steps to manipulate trial data structure
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Trial data structure of a session
+
+    Returns
+    -------
+    df : pd. DataFrame
+        Trial data with operations performed
+
+    """
+    time_signals = [
+        signal for signal in pyal.get_time_varying_fields(df) if "spikes" in signal
+    ]
+
+    # Remove low firing neurons
+    for signal in time_signals:
+        df = pyal.remove_low_firing_neurons(df, signal, 1)
+
+    # Select trials
+    df = pyal.select_trials(df, "trial_name == 'trial'")  # Remove baseline
+
+    # Combine time bins
+    assert np.all(df.bin_size == 0.01), "bin size is not consistent!"
+    df = pyal.combine_time_bins(df, int(Params.BIN_SIZE / 0.01))
+    logger.info(f"Combined every {int(Params.BIN_SIZE / 0.01)} bins")
+
+    # Sqrt transformation for homoscedasticity
+    for signal in time_signals:
+        df = pyal.sqrt_transform_signal(df, signal)
+
+    # Transformation into firing rates
+    df = pyal.add_firing_rates(df, "smooth", std=0.05)
+    for signal in time_signals:
+        logger.info(
+            f"Resulting {signal} ephys data shape is (NxT): {df[signal][0].T.shape}"
+        )
+
+    return df
