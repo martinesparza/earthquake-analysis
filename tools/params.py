4a7ba1e4780050ae6af0aa6a22622cc361a9d569
ioanalzr <ioana.lazar20@imperial.ac.uk>
2025-01-29 14:19:15 +0000

    added colors

--- tools/params.py
+++ tools/params.py
@@ -4,3 +4,10 @@ from dataclasses import dataclass
 @dataclass
 class Params:
     BIN_SIZE = 0.03  # Desired bin size for analysis.
+
+@dataclass
+class colors:
+    M1 = 'r'
+    Dls = 'g'
+    all = 'k'
+    corr_cmap = 'viridis'
