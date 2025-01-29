10a974a840c847502e29b8dc2491a00c06a35e9f
ioanalzr <ioana.lazar20@imperial.ac.uk>
2025-01-29 10:42:22 +0000

    defined colors

--- tools/params.py
+++ tools/params.py
@@ -4,9 +4,3 @@ from dataclasses import dataclass
 @dataclass
 class Params:
     BIN_SIZE = 0.03  # Desired bin size for analysis.
-    
-@dataclass
-class colors:
-    M1 = 'tab:blue'
-    Dls = 'tab:orange'
-    corr_cmap = 'viridis'
