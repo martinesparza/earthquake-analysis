f261e18ae8a1a107aa420818a36abd23dfecd5f6
ioanalzr <ioana.lazar20@imperial.ac.uk>
2025-02-03 16:07:33 +0000

    added epochs

--- tools/params.py
+++ tools/params.py
@@ -1,9 +1,30 @@
 from dataclasses import dataclass
+import pyaldata as pyal
+
 
 
 @dataclass
 class Params:
     BIN_SIZE = 0.03  # Desired bin size for analysis.
+    WINDOW_perturb = (0,0.5)
+    perturb_epoch = pyal.generate_epoch_fun(start_point_name='idx_sol_on',
+                                        rel_start=int(WINDOW_perturb[0]/BIN_SIZE),
+                                        rel_end=int(WINDOW_perturb[1]/BIN_SIZE)
+                                        )
+
+    sol_dir_to_level = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0, 
+        5: 0,
+        6: 1,
+        7: 1,
+        8: 1,
+        9: 1,
+        10: 1
+    }
 
 @dataclass
 class colors:
@@ -11,3 +32,4 @@ class colors:
     Dls = 'g'
     all = 'k'
     corr_cmap = 'viridis'
+
