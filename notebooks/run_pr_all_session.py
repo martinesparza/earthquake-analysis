0180f6037dbb9eeefcbe29c023f0dc88a0ab536f
ioanalzr <ioana.lazar20@imperial.ac.uk>
2026-04-16 15:21:11 +0100

    pr

---
+++ notebooks/run_pr_all_session.py
@@ -0,0 +1,82 @@
+import pickle
+import sys
+
+import numpy as np
+import pyaldata as pyal
+
+sys.path.append("../")
+
+import tools.dimensionality.participation as part
+from tools.dsp.preprocessing import preprocess
+from tools.params import Params
+from tools import dataTools as dt
+prs_with_variable_neurons = {}
+areas = ["MOp", "SSp", "CP", "VAL","MOs"]
+
+sessions = [
+    # "M046_2024_12_18_16_00",
+    # "M046_2024_12_19_13_30",
+    # "M061_2025_03_05_14_00",
+    "M061_2025_03_06_14_00",
+    # "M062_2025_03_19_14_00",
+    # "M062_2025_03_20_14_00",
+    "M062_2025_03_21_14_00",
+    # "M063_2025_03_12_14_00",
+    # "M063_2025_03_13_14_00",
+    "M063_2025_03_14_15_30",
+    "M078_2025_08_06_15_30",
+]
+
+# ========================================== Linear ===========================================
+
+results_path = "/home/il620/earthquake-analysis/notebooks"
+step_size = 10
+n_iter = 20
+
+for session in sessions:
+    prs_with_variable_neurons[session] = {}
+    df = dt.load_sessions([session], prep = True, only_trials = False)[0]
+
+    for area in areas:
+        print(f"Processing area {area}")
+        if f"{area}_rates" not in df.columns:
+            continue
+
+        prs_with_variable_neurons[session][area] = {}
+        for condition in ["free0", "intertrial", "trial","free1"]:
+            print(f"\tProcessing condition {condition}")
+            tmp_df = pyal.select_trials(df, df.trial_name == condition)
+
+
+            if condition == "intertrial" or condition == "free0" or condition == "free1":
+                prs_with_variable_neurons[session][area][condition] = {}
+
+                rates = pyal.concat_trials(tmp_df, f"{area}_rates")
+                prs_with_variable_neurons[session][area][condition]["neurons"] = np.arange(
+                    5, rates.shape[1] + 1, step_size
+                )
+                prs_with_variable_neurons[session][area][condition]["PR"] = (
+                    part.get_pr_for_subsets_of_neurons(rates)
+                )
+            elif condition == "trial":
+                prs_with_variable_neurons[session][area][condition] = {}
+
+                tmp_df = pyal.restrict_to_interval(
+                    tmp_df, epoch_fun=Params.perturb_epoch
+                )
+                rates = pyal.concat_trials(tmp_df, f"{area}_rates")
+                prs_with_variable_neurons[session][area][condition]["neurons"] = np.arange(
+                    5, rates.shape[1] + 1, step_size
+                )
+                prs_with_variable_neurons[session][area][condition]["PR"] = (
+                    part.get_pr_for_subsets_of_neurons(rates)
+                )
+            
+        with open(results_path + "pr_all_sess_diff_num_neurons_perturb.pkl", "wb") as f:
+            pickle.dump(prs_with_variable_neurons, f)
+    del df
+
+            
+
+
+
