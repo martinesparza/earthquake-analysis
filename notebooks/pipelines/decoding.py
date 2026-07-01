4abfa4aee263c823a0bde9a5eb9f4545f85b9eb5
Martin Esparza <martinesparza22.me@gmail.com>
2026-07-01 16:56:45 +0200

    working on jupyterhub branch

--- notebooks/pipelines/decoding.py
+++ notebooks/pipelines/decoding.py
@@ -7,6 +7,7 @@ import numpy as np
 import pyaldata as pyal
 import sys
 
+
 import scipy
 from sklearn.linear_model import RidgeCV
 
--- tools/dsp/preprocessing.py
+++ tools/dsp/preprocessing.py
@@ -132,7 +132,7 @@ def preprocess(
 
 def load_and_process_session(
     session,
-    data_dir="/data/bnd-data/raw/",
+    data_dir="/home/jovyan/mouse-data-london/proc/",
     bhv_fields=None,
     min_immobile_bins=5,
     rates=True,
